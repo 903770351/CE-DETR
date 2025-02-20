6859a9f01f2b284051a97eb5e7adab6738bc9c54
903770351 <115074385+903770351@users.noreply.github.com>
2025-02-20 14:42:20 +0800

    Add files via upload

--- util/visualizer.py
+++ util/visualizer.py
@@ -110,7 +110,8 @@ class COCOVisualizer():
             poly = [[bbox_x, bbox_y], [bbox_x, bbox_y+bbox_h], [bbox_x+bbox_w, bbox_y+bbox_h], [bbox_x+bbox_w, bbox_y]]
             np_poly = np.array(poly).reshape((4,2))
             polygons.append(Polygon(np_poly))
-            c = (np.random.random((1, 3))*0.6+0.4).tolist()[0]
+            # c = (np.random.random((1, 3))*0.6+0.4).tolist()[0]
+            c = (1.0, 0.0, 0.0)
             color.append(c)
 
         p = PatchCollection(polygons, facecolor=color, linewidths=0, alpha=0.1)
